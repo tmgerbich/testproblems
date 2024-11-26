b652103dae77817aa83177dc18ed44f9c62c0712
tmgerbich <therese.m.gerbich@gmail.com>
2024-11-26 10:47:39 -0500

    new problem

--- removeelement.js
+++ removeelement.js
@@ -1,4 +1,4 @@
-// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed.
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 let nums;
 let val;
@@ -9,8 +9,8 @@ val = 3;
 k = 0;
 
 for (let i=0; i<nums.length; i++){
-    if (nums[i] == val){
-        nums[i] = '_';
+    if (nums[i] !== val){
+        nums[k] = nums [i];
         k++;
     }
 }
