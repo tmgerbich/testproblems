d9e16f7770eca6882efa8d9fb54f5c1f4cbd1c7c
tmgerbich <therese.m.gerbich@gmail.com>
2024-12-04 10:16:15 -0500

    fixed space

--- testproblems.js
+++ testproblems.js
@@ -1,5 +1,5 @@
 // Given an array of integers nums and an integer target, 
-// return indices of the two numbers such that they add up to the target.
+// return indices of the two numbers such that they add up to target.
 
 let nums
 nums = [1, 2, 3];
