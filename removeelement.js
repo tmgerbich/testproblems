16ef866e0750cc73a7866c6e65325ee6f72544cd
tmgerbich <therese.m.gerbich@gmail.com>
2024-12-04 10:15:46 -0500

    prob

--- removeelement.js
+++ removeelement.js
@@ -1,4 +1,5 @@
-// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+// The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 let nums;
 let val;
